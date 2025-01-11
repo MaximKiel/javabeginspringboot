b849968d3e6da8f2e513cd204e09063bbbde8bda
MaximKiel <loup.du.nord@yandex.ru>
2025-01-11 11:15:27 +0300

    Comment test

--- src/test/java/ru/javabegin/todobackend/TodoBackendApplicationTests.java
+++ src/test/java/ru/javabegin/todobackend/TodoBackendApplicationTests.java
@@ -1,12 +1,12 @@
 package ru.javabegin.todobackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TodoBackendApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
