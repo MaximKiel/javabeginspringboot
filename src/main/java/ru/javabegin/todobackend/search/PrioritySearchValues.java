9bbee5f0288f243782ee9974239e13295683625d
MaximKiel <loup.du.nord@yandex.ru>
2025-01-24 20:31:13 +0300

    Add repository, service, search values and controller for Priority

---
+++ src/main/java/ru/javabegin/todobackend/search/PrioritySearchValues.java
@@ -0,0 +1,15 @@
+package ru.javabegin.todobackend.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PrioritySearchValues {
+    private String title;
+    private String email;
+}
